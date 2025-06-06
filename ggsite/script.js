50e6e84fd75434cca5a53bb2ce70a03e1198b1a5
whopperflower13 <142776447+whopperflower13@users.noreply.github.com>
2025-06-06 23:32:06 +1000

    button update

---
+++ ggsite/script.js
@@ -0,0 +1,18 @@
+const viewBtn = document.querySelector("#view-btn");
+console.log(viewBtn);
+
+// const playImg = document.querySelector("#play-button-img");
+// console.log(playImg);
+
+// playPauseBtn.addEventListener("click", togglePlay);
+// // playPauseBtn.addEventListener("click", updateCountdown);
+
+// function togglePlay() {
+//   if (myAudio.paused || myAudio.ended) {
+//     myAudio.play();
+//     playImg.src = "stop-button.PNG";
+//   } else {
+//     myAudio.pause();
+//     playImg.src = "play-button.PNG";
+//   }
+// }
