d7ae69712a91595e051d869720ca86098af00728
whopperflower13 <142776447+whopperflower13@users.noreply.github.com>
2025-06-14 19:43:29 +1000

    blog page

---
+++ ggsite/blogpost/script.js
@@ -0,0 +1,32 @@
+const viewBtn = document.querySelector("#view-btn")
+console.log(viewBtn)
+
+let currentlocation = window.location
+console.log(currentlocation)
+
+const openLink = (url) => {
+    window.location.href = url
+} 
+
+viewBtn.addEventListener("click", () => {
+    console.log("button clicked!")
+
+    openLink('../index.html')
+})
+
+
+// const playImg = document.querySelector("#play-button-img");
+// console.log(playImg);
+
+// playPauseBtn.addEventListener("click", togglePlay);
+// // playPauseBtn.addEventListener("click", updateCountdown);
+
+// function togglePlay() {
+//   if (myAudio.paused || myAudio.ended) {
+//     myAudio.play();
+//     playImg.src = "stop-button.PNG";
+//   } else {
+//     myAudio.pause();
+//     playImg.src = "play-button.PNG";
+//   }
+// }
